dcf37ebdc0e6d2ce4259d7a151df61021f40f256
piyushjain5350 <126599802+piyushjain5350@users.noreply.github.com>
2023-07-01 01:17:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 function highlight() {
     //Write your code here
-
+	const target=document.getElementsByTagName("strong");
+	
+	const targetArray = Array.from(target); // Convert HTMLCollection to array
+    
+    targetArray.forEach((element) => {
+        element.classList.add("color-change")
+    });
 
 }
 
+const para=document.getElementByTagName("p")[0];
+para.addEventListener("hover",highlight());
 
 function return_normal() {
     //Write your code here
-
+	const target=document.getElementsByTagName("strong");
+	
+	const targetArray = Array.from(target); 
     
+    targetArray.forEach((element) => {
+        element.classList.remove("color-change")
+    });
+  
 }
